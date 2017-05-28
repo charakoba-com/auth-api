2e214b70b961f7d1c062eda9d8d39770ef5e7dae
nasa9084 <nasa9084@live.jp>
2017-05-28 23:55:43 +0900

    add: healthCheckHandler test

---
+++ authapi_test.go
@@ -0,0 +1,38 @@
+package authapi_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	authapi "github.com/charakoba-com/auth-api"
+)
+
+func TestHealthCheckHandler(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(authapi.HealthCheckHandler))
+	defer ts.Close()
+
+	buf := bytes.Buffer{}
+	json.NewEncoder(&buf).Encode(map[string]string{
+		"message": "hello, world",
+	})
+
+	r, err := http.Get(ts.URL)
+	if err != nil {
+		t.Errorf("%s", err)
+		return
+	}
+
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		t.Errorf("%s", err)
+		return
+	}
+	if string(data) != buf.String() {
+		t.Errorf("%s != %s", data, buf.String())
+		return
+	}
+}
---
+++ export_test.go
@@ -0,0 +1,10 @@
+package authapi
+
+var HealthCheckHandler = healthCheckHandler
+var PostUserHandler = postUserHandler
+var DeleteUserHandler = deleteUserHandler
+var GetUserListHandler = getUserListHandler
+var PostAuthHandler = postAuthHandler
+var GetAlgorithmHandler = getAlgorithmHandler
+var PostVerifyHandler = postVerifyHandler
+var GetKeyHandler = getKeyHandler
