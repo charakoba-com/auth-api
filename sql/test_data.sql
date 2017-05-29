e10617a23e847969ba10abad8270876c52acafce
nasa9084 <nasa9084@live.jp>
2017-05-29 13:14:43 +0900

    fix: test data insersion table name is wrong

--- sql/test_data.sql
+++ sql/test_data.sql
@@ -1,2 +1,2 @@
 -- data insertion for tests
-INSERT INTO apidb (username, password, created_on, modified_on) VALUES ('testuser', 'testpasswd', '2017-01-01 00:00:00+9', '2017-01-01 00:00:00+9');
+INSERT INTO users (username, password, created_on, modified_on) VALUES ('testuser', 'testpasswd', '2017-01-01 00:00:00+9', '2017-01-01 00:00:00+9');
