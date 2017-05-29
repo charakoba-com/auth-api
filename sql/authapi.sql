44b91a30b26185ed829044d178154f2670049856
nasa9084 <nasa9084@live.jp>
2017-05-29 10:56:37 +0900

    fix: mysql's unique key is limited max 767 bytes
    256 chars on utf8mb4 is 768 bytes

--- sql/authapi.sql
+++ sql/authapi.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS users;
 
 -- Create new tables
 CREATE TABLE users (
-        username VARCHAR(256) NOT NULL,
+        username VARCHAR(128) NOT NULL,
         password VARCHAR(1024) NOT NULL,
         created_on DATETIME NOT NULL,
         modified_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
