b980f005ab87cf0c24a422e604b64748fbdaf82c
nasa9084 <nasa9084@live.jp>
2017-05-29 14:36:38 +0900

    fix: testdata

--- sql/test_data.sql
+++ sql/test_data.sql
@@ -1,3 +1,4 @@
 -- data insertion for tests
 INSERT INTO users (username, password, created_on, modified_on) VALUES ('testuser', 'testpasswd', '2017-01-01 00:00:00+9', '2017-01-01 00:00:00+9');
 INSERT INTO users (username, password, created_on) VALUES('updateuser', 'testpasswd', '2017-01-01 00:00:00+9');
+INSERT INTO users (username, password, created_on) VALUES('deleteuser', 'testpasswd', '2017-01-01 00:00:00+9');
