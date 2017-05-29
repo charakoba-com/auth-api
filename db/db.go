c288c1d45c81cd756c8845dc6e81cabe03e33d0a
nasa9084 <nasa9084@live.jp>
2017-05-29 10:33:31 +0900

    add: database connection

---
+++ db/db.go
@@ -0,0 +1,45 @@
+package db
+
+import (
+	"database/sql"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/pkg/errors"
+)
+
+var _db *sql.DB // global database connection
+
+// Init database connection
+func Init(c *mysql.Config) (err error) {
+	dn := "mysql"
+
+	if c == nil {
+		c = &mysql.Config{
+			User:      "root",
+			Net:       "tcp",
+			Addr:      "127.0.0.1:3306",
+			DBName:    "apidb",
+			ParseTime: true,
+		}
+	}
+
+	dsn := c.FormatDSN()
+
+	_db, err = sql.Open(dn, dsn)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// BeginTx returns a transaction
+func BeginTx() (*sql.Tx, error) {
+	if _db == nil {
+		return nil, errors.New(`database connection has not been initialized`)
+	}
+	tx, err := _db.Begin()
+	if err != nil {
+		return nil, errors.Wrap(err, `begining transaction`)
+	}
+	return tx, nil
+}
